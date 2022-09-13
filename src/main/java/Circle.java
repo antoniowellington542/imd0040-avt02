2b9d2842fb906c73b258b5997604400851cf856c
antoniowellington542 <antoniowellington542@gmail.com>
2022-09-13 13:44:00 -0300

    feat: area and circumference methods

---
+++ src/main/java/Circle.java
@@ -0,0 +1,19 @@
+class Circle {
+    float radius;
+
+
+    Circle(float radius) {
+        this.radius = radius;
+    }
+
+    double area(){
+        double result = 3.14 * Math.pow(radius, 2);
+        return  result;
+    }
+
+    double circumference(){
+        double r = 3.11;
+        return r;
+    }
+
+}
